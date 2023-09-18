b0c040f7172a4b77f53a773c2a074a6056931c58
@Halfdan.Hesthammer <@Halfdan.Hesthammer>
2023-09-18 15:17:22 +0200

    får input og omgjør til koffein

---
+++ app/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+from rdkit import Chem
+from rdkit.Chem import Descriptors, Draw
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/identify_molecule', methods=['POST'])
+def identify_molecule():
+    smiles = request.form.get('inputField', '')
+    
+    molecule = Chem.MolFromSmiles(smiles)
+    
+    if molecule is None:
+        return render_template('index.html', molecule_name="Invalid SMILES")
+    
+    molecule_weight = Descriptors.ExactMolWt(molecule)
+    return render_template('index.html', molecule_name=molecule_weight)
+
+if __name__ == '__main__':
+    app.run(debug=True)
