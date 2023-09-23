e7407bd20bdc4ce6ceb448f9a4b91f0f5bc452f9
@Halfdan.Hesthammer <@Halfdan.Hesthammer>
2023-09-23 20:05:10 +0200

    routes

--- app/app.py
+++ app/app.py
@@ -1,31 +1,12 @@
-from flask import Flask, render_template, request
-from rdkit import Chem
-from rdkit.Chem import Descriptors
+from flask import Flask, render_template
+from app.routes import main  # Replace 'your_folder_name' with the actual folder name where routes.py is located
 
 app = Flask(__name__)
+app.register_blueprint(main)
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
-
-@app.route('/identify_molecule', methods=['POST'])
-def identify_molecule():
-    smiles = request.form.get('inputField', '')
-    selected_options = request.form.getlist('displayOptions')
-    
-    molecule = Chem.MolFromSmiles(smiles)
-    
-    descriptors = {}
-    if molecule is not None:
-        if 'MolecularWeight' in selected_options:
-            descriptors['MolecularWeight'] = Descriptors.ExactMolWt(molecule)
-        if 'PSA' in selected_options:
-            descriptors['PSA'] = Descriptors.TPSA(molecule)
-        # Add other descriptors here based on selected_options
-    else:
-        descriptors['Error'] = "Invalid SMILES"
-    
-    return render_template('index.html', descriptors=descriptors)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/routes.py
+++ app/routes.py
@@ -2,3 +2,27 @@
 
 # /calculate: Accepts the molecule string and returns the calculated descriptors.
 # /download: Provides the descriptors as a downloadable .csv file.
+from flask import Blueprint, render_template, request
+from rdkit import Chem
+from rdkit.Chem import Descriptors
+
+main = Blueprint('main', __name__)
+
+@main.route('/identify_molecule', methods=['POST'])
+def identify_molecule():
+    smiles = request.form.get('inputField', '')
+    selected_options = request.form.getlist('displayOptions')
+    
+    molecule = Chem.MolFromSmiles(smiles)
+    
+    descriptors = {}
+    if molecule is not None:
+        if 'MolecularWeight' in selected_options:
+            descriptors['MolecularWeight'] = Descriptors.ExactMolWt(molecule)
+        if 'PSA' in selected_options:
+            descriptors['PSA'] = Descriptors.TPSA(molecule)
+        # Add other descriptors here based on selected_options
+    else:
+        descriptors['Error'] = "Invalid SMILES"
+    
+    return render_template('index.html', descriptors=descriptors)
