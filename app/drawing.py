41e989bc3fa65bb061c1b65289add8df3df4cf4f
linus.raaen <zec018@uib.no>
2023-11-07 01:54:40 +0100

    drawing tool file un-commented

--- app/drawing.py
+++ app/drawing.py
@@ -1,7 +1,7 @@
-#import streamlit as st
+import streamlit as st
 
-#from streamlit_ketcher import st_ketcher
+from streamlit_ketcher import st_ketcher
 
-#molecule = st.text_input("Molecule", "CCO")
-#smile_code = st_ketcher(molecule)
-#st.markdown(f"Smile code: ``{smile_code}``")
+molecule = st.text_input("Molecule", "CCO")
+smile_code = st_ketcher(molecule)
+st.markdown(f"Smile code: ``{smile_code}``")
